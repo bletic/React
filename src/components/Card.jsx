b5c802ec790252cf99fc4c5874f36db1692c477e
Branislav <bletic33@gmail.com>
2024-04-23 15:50:19 +0200

    prvi commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({children, bg= 'bg-blue-400' }) => {
+  return (
+    <div className= {`${bg} p-6 rounded-lg shadow-md`}> 
+        {children}
+    </div>   
+  )
+}
+
+export default Card
+
