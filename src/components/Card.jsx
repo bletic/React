7565f405fb904ae2fc2389ab2d35a15ec0578b02
Letić Branislav <46036029+bletic@users.noreply.github.com>
2024-05-31 11:19:10 +0200

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,7 @@ const Card = ({children, bg= 'bg-blue-400' }) => {
   return (
     <div className= {`${bg} p-6 rounded-lg shadow-md`}> 
         {children}
+      <p> Baneto </p>
     </div>   
   )
 }
